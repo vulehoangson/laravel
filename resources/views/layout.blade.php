e0dc8aaac642c501a837b7d8b61c26baee1f5328
sonvlh <vulehoangson1995@github.com>
2018-01-02 18:22:58 +0700

    update upload file

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,8 @@
         @yield('title')
     </title>
 
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    {{--<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">--}}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/magnific-popup.css') }}">
     <link href="{{ asset('css/jquery-ui.min.css') }}" rel="stylesheet" >
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -3,7 +3,30 @@
 @section('title','Tạo bài đăng')
 
 @section('content')
-
+    <style rel="stylesheet">
+        .image-preview-input {
+            position: relative;
+            overflow: hidden;
+            margin: 0px;
+            color: #333;
+            background-color: #fff;
+            border-color: #ccc;
+        }
+        .image-preview-input input[type=file] {
+            position: absolute;
+            top: 0;
+            right: 0;
+            margin: 0;
+            padding: 0;
+            font-size: 20px;
+            cursor: pointer;
+            opacity: 0;
+            filter: alpha(opacity=0);
+        }
+        .image-preview-input-title {
+            margin-left:2px;
+        }
+    </style>
     <div class="title" style="padding-bottom: 15px;border-bottom: 1px solid #dddddd;margin-bottom: 20px;">
         <h2>Tạo bài đăng</h2>
     </div>
@@ -69,6 +92,22 @@
             </div>
 
 
+            <div class="input-group image-preview" style="width: 25%;margin-bottom: 20px;">
+                <input type="text" class="form-control image-preview-filename" disabled="disabled"> <!-- don't give a name === doesn't send on POST/GET -->
+                <span class="input-group-btn">
+                    <!-- image-preview-clear button -->
+                    <button type="button" class="btn btn-default image-preview-clear" style="display:none;margin-top: 10px;height: 40px;">
+                        <span class="glyphicon glyphicon-remove"></span> Clear
+                    </button>
+                    <!-- image-preview-input -->
+                    <div class="btn btn-default image-preview-input" style="margin-top: 10px;height: 40px;">
+                        <span class="glyphicon glyphicon-folder-open" style="padding-top: 5px;"></span>
+                        <span class="image-preview-input-title">Browse</span>
+                        <input type="file" accept="image/png, image/jpeg, image/gif" name="input-file-preview"/> <!-- rename it -->
+                    </div>
+                </span>
+            </div><!-- /input-group image-preview [TO HERE]-->
+
 
             <div>
                 <button class="btn btn-danger" id="submit" value="login">Tạo</button>
@@ -91,6 +130,67 @@
             {
                 $( "div.success" ).fadeIn( 500 ).delay( 1500 ).fadeOut( 500 );
             }
+        });
+
+        $(document).on('click', '#close-preview', function(){
+            $('.image-preview').popover('hide');
+            // Hover befor close the preview
+            $('.image-preview').hover(
+                    function () {
+                        $('.image-preview').popover('show');
+                    },
+                    function () {
+                        $('.image-preview').popover('hide');
+                    }
+            );
+        });
+
+        $(function() {
+            // Create the close button
+            var closebtn = $('<button/>', {
+                type:"button",
+                text: 'x',
+                id: 'close-preview',
+                style: 'font-size: initial;',
+            });
+            closebtn.attr("class","close pull-right");
+            // Set the popover default content
+            $('.image-preview').popover({
+                trigger:'manual',
+                html:true,
+                title: "<strong>Preview</strong>"+$(closebtn)[0].outerHTML,
+                content: "There's no image",
+                placement:'bottom'
+            });
+            // Clear event
+            $('.image-preview-clear').click(function(){
+                $('.image-preview').attr("data-content","").popover('hide');
+                $('.image-preview-filename').val("");
+                $('.image-preview-clear').hide();
+                $('.image-preview-input input:file').val("");
+                $(".image-preview-input-title").text("Browse");
+                $('.image-preview').css('width','25%');
+            });
+            // Create the preview image
+            $(".image-preview-input input:file").change(function (){
+                $('.image-preview').css('width','30%');
+                var img = $('<img/>', {
+                    id: 'dynamic',
+                    width:250,
+                    height:200
+                });
+                var file = this.files[0];
+                var reader = new FileReader();
+                // Set preview image into the popover data-content
+                reader.onload = function (e) {
+                    $(".image-preview-input-title").text("Change");
+                    $(".image-preview-clear").show();
+                    $(".image-preview-filename").val(file.name);
+                    img.attr('src', e.target.result);
+                    $(".image-preview").attr("data-content",$(img)[0].outerHTML).popover("show");
+                }
+                reader.readAsDataURL(file);
+            });
         });
     </script>
 @endsection
